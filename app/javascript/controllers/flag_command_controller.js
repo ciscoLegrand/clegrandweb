1d78e2edee49d157445bfd70b28a8b9285e999ae
ciscoLegrand <cisco.glez@gmail.com>
2023-10-19 23:20:26 +0200

    style terminal

--- app/javascript/controllers/flag_command_controller.js
+++ app/javascript/controllers/flag_command_controller.js
@@ -1,28 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["input", "output", "preset", "database", "housekeeping", "pipeline","javascript", "css", "goodies", "tests"]
+  static targets = ['input', 'output', 'preset', 'database', 'housekeeping', 'pipeline','javascript', 'css', 'goodies', 'tests']
 
   connect() {
-    console.log("FlagCommandController connected!")
-    this.updateOutput();  // Llama a updateOutput en la conexión para establecer el comando inicial
+    console.log('FlagCommandController connected!')
+    this.updateOutput()
   }
 
   updateOutput() {
-    const appName = this.getAppName();
-    const presetCommand = this.getPresetCommand();
-    const databaseCommand = this.getDatabaseCommand();
-    const housekeepingCommand = this.getHousekeepingCommand();
-    const pipelineCommand = this.getPipelineCommand();
-    const javascriptCommand = this.getJavascriptCommand();
-    const cssCommand = this.getCssCommand();
-    const activeCommand = this.getActiveCommand();
-    const testCommand = this.getTestCommand();
+    const appName = this.getAppName()
+    const presetCommand = this.getPresetCommand()
+    const databaseCommand = this.getDatabaseCommand()
+    const housekeepingCommand = this.getHousekeepingCommand()
+    const pipelineCommand = this.getPipelineCommand()
+    const javascriptCommand = this.getJavascriptCommand()
+    const cssCommand = this.getCssCommand()
+    const activeCommand = this.getActiveCommand()
+    const testCommand = this.getTestCommand()
     this.outputTarget.innerHTML = `
-      <span class='text-green-400'>$ </span>
-      <span class='text-blue-400'>rails new</span> 
-      <span class='text-red-400'>${appName} </span>
-      <span class='text-yellow-700'>
+      <span class='text-red-600'>$ </span>
+      <span class='text-pink-600'>rails new</span> 
+      <span class='text-pink-500'>${appName} </span>
+      <span class='text-pink-300'>
         ${presetCommand}
         ${databaseCommand}
         ${housekeepingCommand}
@@ -36,83 +36,76 @@ export default class extends Controller {
   }
 
   getAppName() {
-    return this.inputTarget.value;
+    return this.inputTarget.value
   }
 
   getPresetCommand() {
-    const selectedPreset = this.presetTargets.find(radio => radio.checked);
+    const selectedPreset = this.presetTargets.find(radio => radio.checked)
     console.log(selectedPreset.value)
     if (selectedPreset.value === 'preset_none') { return '' }
     return `--${selectedPreset.value}`
   }
 
   getDatabaseCommand() {
-    const selectedDatabase = this.databaseTargets.find(radio => radio.checked);
-    return selectedDatabase ? `--database=${selectedDatabase.value}` : '';
+    const selectedDatabase = this.databaseTargets.find(radio => radio.checked)
+    return selectedDatabase ? `--database=${selectedDatabase.value}` : ''
   }
 
   getHousekeepingCommand() {
-    console.log("Housekeeping targets:", this.housekeepingTargets);
-    const defaultSkips = ['gitignore', 'keeps', 'bootsnap', 'bundle'];
-    const checkedFlags = this.housekeepingTargets.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    console.log('Housekeeping targets:', this.housekeepingTargets)
+    const defaultSkips = ['gitignore', 'keeps', 'bootsnap', 'bundle']
+    const checkedFlags = this.housekeepingTargets.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value)
   
-    console.log("Checked flags:", checkedFlags);
+    console.log('Checked flags:', checkedFlags)
     const skipCommands = defaultSkips
       .filter(flag => !checkedFlags.includes(flag))
       .map(flag => `--skip-${flag}`)
-      .join(' ');
+      .join(' ')
   
-    console.log("Skip commands:", skipCommands);
-    return skipCommands === '' ? '' : (skipCommands || '--skip-gitignore --skip-keeps --skip-bootsnap --skip-bundle');
+    console.log('Skip commands:', skipCommands)
+    return skipCommands === '' ? '' : (skipCommands || '--skip-gitignore --skip-keeps --skip-bootsnap --skip-bundle')
 
   }
 
   getPipelineCommand() {
-    const selectedPipeline = this.pipelineTargets.find(radio => radio.checked);
+    const selectedPipeline = this.pipelineTargets.find(radio => radio.checked)
     if (selectedPipeline.value === 'pipeline_none') { return '--skip-asset-pipeline' }
     if (selectedPipeline.value === 'propshaft') { return '--asset-pipeline=propshaft' }
     return ''
   }
 
   getJavascriptCommand() {
-    const selectedJS = this.javascriptTargets.find(radio => radio.checked);
+    const selectedJS = this.javascriptTargets.find(radio => radio.checked)
     if(selectedJS.value === 'javascript_none') { return '--skip-javascript' }
-    return selectedJS ? `--javascript=${selectedJS.value}` : '';
+    return selectedJS ? `--javascript=${selectedJS.value}` : ''
   }
 
   getCssCommand() {
-    const selectedCss = this.cssTargets.find(radio => radio.checked);
+    const selectedCss = this.cssTargets.find(radio => radio.checked)
     if (selectedCss.value === 'css_none') { return '' }
     return `--css=${selectedCss.value}`
   }
 
   getActiveCommand() {
-    console.log("Goodies targets:", this.goodiesTargets);
-    // Tus skips por defecto
-    const defaultSkips = ['cable', 'job', 'mailbox', 'mailer', 'record', 'storage', 'text', 'hotwire', 'jbuilder'];
-    
-    // Obtiene los flags que están seleccionados
-    const checkedFlags = this.goodiesTargets.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-    
-    // Filtra los skips por defecto basándose en los flags seleccionados
+    console.log('Goodies targets:', this.goodiesTargets)
+    const defaultSkips = ['cable', 'job', 'mailbox', 'mailer', 'record', 'storage', 'text', 'hotwire', 'jbuilder']
+    const checkedFlags = this.goodiesTargets.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value)
     const skipCommands = defaultSkips
       .filter(flag => !checkedFlags.includes(flag))
       .map(flag => `--skip-${flag}`)
-      .join(' ');
-  
-    // Retorna la cadena de comandos; si está vacía, retorna una cadena vacía
-    return skipCommands === '' ? '' : skipCommands;
+      .join(' ')
+    return skipCommands === '' ? '' : skipCommands
   }
 
   getTestCommand() {
-    const defaultSkips = ['test', 'system-test'];
-    const checkedFlags = this.testsTargets.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    const defaultSkips = ['test', 'system-test']
+    const checkedFlags = this.testsTargets.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value)
   
     const skipCommands = defaultSkips
       .filter(flag => !checkedFlags.includes(flag))
       .map(flag => `--skip-${flag}`)
-      .join(' ');
+      .join(' ')
   
-    return skipCommands;
+    return skipCommands
   }
 }
